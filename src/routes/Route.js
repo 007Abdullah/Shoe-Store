bdde028c4696a7c75a5aa9a9994cf67322cb49b3
007Abdullah <kb337137@gmail.com>
2021-06-10 08:55:49 +0500

    shoe store

---
+++ src/routes/Route.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './../components/Home';
+import Products from './../components/Products';
+import About from './../components/About';
+
+
+export default function Route() {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
