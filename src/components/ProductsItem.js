254685101127079feb2d9fd1027f26cab21fa1b2
007Abdullah <kb337137@gmail.com>
2021-06-10 09:54:08 +0500

    Routes maintain

--- src/components/ProductsItem.js
+++ src/components/ProductsItem.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-
-export default function Navbar() {
-
+export default function ProductsItem() {
     return (
         <React.Fragment>
-            
+
         </React.Fragment>
     )
 }
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import Home from './../components/Home';
 import Products from './../components/Products';
+import ProductsItem from './../components/ProductsItem';
 import About from './../components/About';
+import Contact from './../components/Contact';
+import Navbar from './../components/Navbar';
 
 
-export default function Route() {
+
+export default function Routes() {
     return (
         <React.Fragment>
-
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/about" component={About} />
+                    <Route exact path="/products" component={Products} />
+                    <Route path="/products/:id" component={ProductsItem} />
+                    <Route path="*" component={() => <h1>404 Not Found</h1>} />
+                </Switch>
+            </Router>
         </React.Fragment>
     )
 }
